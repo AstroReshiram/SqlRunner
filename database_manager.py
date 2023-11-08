af8d890ff586f6364015065e1ce3dba6029b79d9
AstroReshiram <66257895+AstroReshiram@users.noreply.github.com>
2023-11-08 17:48:56 +0000

    updated connection string

--- database_manager.py
+++ database_manager.py
@@ -8,7 +8,12 @@ class DatabaseManager:
     def __init__(self, server, database):
         self.server = server
         self.database = database
-        self.engine = sa.create_engine('mssql+pymssql://' + server + '/' + database)
+        self.engine = sa.create_engine('mssql+pyodbc://localhost/master?driver=ODBC+Driver+17+for+SQL+Server',
+                                       connect_args={'timeout': 10})
+
+        # using pyodbc mssql+pyodbc://localhost/master?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes&TrustServerCertification=no&Authentication=ActiveDirectoryInteractive
+        # using pymssql mssql+pymssql://username:password@localhost/dbname
+        # mssql+pyodbc://@' + servername + '/' + dbname + '?trusted_connection=yes&driver=ODBC+Driver+13+for+SQL+Server
 
     def read_table(self, table_name):
         """Returns table_name as a pandas dataframe."""
--- sqlcode.sql
+++ sqlcode.sql
@@ -1,3 +1,5 @@
+SET NOCOUNT ON --pyodbc doesn't like it and reports multiple lines. you can uncomment if using pymssql
+
 IF NOT EXISTS (
     SELECT *
     FROM INFORMATION_SCHEMA.TABLES
@@ -14,4 +16,4 @@ END
 INSERT INTO people (name, age, date_registered)
 VALUES ('Guy', 20, '2023-01-01');
 
-SELECT * FROM people;
+SELECT * FROM people
